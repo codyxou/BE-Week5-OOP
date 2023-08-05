ac2314b4e0b39cea66e1a8ec3c4873ac20c53b55
unknown <xoumanivongco@gmail.com>
2023-08-05 00:47:04 -0600

    added comments to files

--- assignment/AsteriskLogger.java
+++ assignment/AsteriskLogger.java
@@ -1,9 +1,10 @@
 package assignment;
 
 public class AsteriskLogger implements Logger {
-
+//this was less tricker to implement compared to space logger. Used a system out to add the asterisks and concatenate with the string variable, log and error respectively. 
 	@Override
 	public void log(String log) {
+		//
 		
 		System.out.println("******************\n" + 
 							"*** " + "Log: " + log + " ***\n" + 
--- assignment/SpacedLogger.java
+++ assignment/SpacedLogger.java
@@ -3,11 +3,13 @@ package assignment;
 public class SpacedLogger implements Logger {
 
 	@Override
+	//using a loop to iterate through the length of the string (log and error in this case). As the string is iterated through, it is being converted as a character into variable c. variable c is logged out to the console and a space is added
 	public void log(String log) {
 		for (int i = 0; i < log.length(); i++) {
 			char c = log.charAt(i);
 			System.out.print(c + " ");
 		}
+		//adding a line break to clean up the code a bit in the console. 
 		System.out.println("\n");
 	}
 
